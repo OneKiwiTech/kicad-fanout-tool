b227e5583e3bc0c61d2ab6a10240f541e4058d75
Van Son <thanh.duong.vs@gmail.com>
2022-08-07 10:46:20 +0700

    add pitch

--- onekiwi/model/bga.py
+++ onekiwi/model/bga.py
@@ -6,6 +6,8 @@ class BGA:
         self.reference = reference
         self.track = track
         self.via = via
+        self.pitchx = 0
+        self.pitchy = 0
 
         self.logger.info(reference)
         self.footprint = self.board.FindFootprintByReference(reference)
@@ -13,37 +15,23 @@ class BGA:
         self.pads = self.footprint.Pads()
         self.x0 = self.footprint.GetPosition().x
         self.y0 = self.footprint.GetPosition().y
-        self.logger.info('aaaaaaaaaaaaaaaaaa')
         self.init_data()
     
     def init_data(self):
         if self.angle not in [0.0 , 90.0, 180.0, -90.0]:
             self.footprint.SetOrientationDegrees(0)
-            self.logger.info('b3')
-        arr_xy = []
-        #name_xy = []
-        
-        pitch_x = 0
-        pitch_y = 0
+        pos_x = []
+        pos_y = []
+
         minx = self.pads[0].GetPosition().x
         maxx = self.pads[0].GetPosition().x
         miny = self.pads[0].GetPosition().y
         maxy = self.pads[0].GetPosition().y
-        temps = []
-        #name_temp = []
-        #name_temp.append(self.pads[0].GetPadName())
-        #name_xy.append(name_temp)
-        self.logger.info('b1')
-        temps.append(self.pads[0].GetPosition())
-        arr_xy.append(temps)
-        self.logger.info('b2')
-        a = arr_xy[0]
-        self.logger.info(arr_xy)
-        self.logger.info(a)
-        self.logger.info(a[0])
-        self.logger.info(a[0].x)
+
+        pos_x.append([self.pads[0].GetPosition()])
+        pos_y.append([self.pads[0].GetPosition()])
         
-        for ind, pad in enumerate(self.pads, 1):
+        for pad in self.pads:
             pos = pad.GetPosition()
             if minx > pos.x:
                 minx = pos.x
@@ -53,23 +41,49 @@ class BGA:
                 miny = pos.y
             if maxy < pos.y:
                 maxy = pos.y
+            checkx = True
+            for arr in pos_x:
+                if arr[0].y == pos.y and pos not in arr:
+                    checkx = False
+                    arr.append(pos)
+            if checkx == True:
+                pos_x.append([pos])
             
-            for arrx in arr_xy:
-                self.logger.info('b4')
-                self.logger.info('%d - %d' %(arrx[0].y, pos.y))
-                if arrx[0].y == pos.y:
-                    arrx.append(pos)
-                    self.logger.info('b5')
-                    """
-                else:
-                    self.logger.info('b6')
-                    temp = []
-                    temp.append(pos)
-                    arr_xy.append(temp)
-                    self.logger.info('b7')
-            """
-        self.logger.info('bn')
-        for arr in arr_xy:
-            self.logger.info('---------------------')
-            self.logger.info('%s' %str(arr))
+            checky = True
+            for arr in pos_y:
+                if arr[0].x == pos.x and pos not in arr:
+                    checky = False
+                    arr.append(pos)
+            if checky == True:
+                pos_y.append([pos])
+            
+        for arrs in pos_x:
+            arrs.sort(key=lambda x:x.x)
+        for arrs in pos_y:
+            arrs.sort(key=lambda x:x.y)
+        
+        self.pitchx = pos_x[0][1].x - pos_x[0][0].x
+        for arrs in pos_x:
+            for i in range(len(arrs)):
+                if i > 0:
+                    pitch = arrs[i].x - arrs[i-1].x
+                    if pitch > 0 and pitch < self.pitchx:
+                        self.pitchx = pitch
+        
+        self.pitchy = pos_y[0][1].y - pos_y[0][0].y
+        for arrs in pos_y:
+            for i in range(len(arrs)):
+                if i > 0:
+                    pitch = arrs[i].y - arrs[i-1].y
+                    if pitch > 0 and pitch < self.pitchy:
+                        self.pitchy = pitch
+        self.logger.info('pitch x: %d' %self.pitchx)
+        self.logger.info('pitch y: %d' %self.pitchy)
+        """
+        for ind, arrs in enumerate(pos_y):
+            self.logger.info('%d. sort---------------------' %ind)
+            for i, arr in enumerate(arrs):
+                self.logger.info('%d. %s' %(i, str(arr)))
+        """
+        
         self.footprint.SetOrientationDegrees(self.angle)
