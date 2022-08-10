22cd4144509ca60cc7e371461d52ea35455074f9
Van Son <thanh.duong.vs@gmail.com>
2022-08-10 14:02:24 +0700

    update 2022.08.11

--- onekiwi/plugin.py
+++ onekiwi/plugin.py
@@ -6,7 +6,7 @@ class LengthMatchingAction(pcbnew.ActionPlugin):
 	def defaults(self):
 		self.name = "Fanout Tools"
 		self.category = "Modify PCB"
-		self.description = "Edit reference value on layer Silkscreen and Fabrication"
+		self.description = "BGA fanout routing"
 		self.show_toolbar_button = True # Optional, defaults to False
 		self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png') # Optional
 
--- onekiwi/version.py
+++ onekiwi/version.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 
-LAST_TAG = '2022.08.10'
+LAST_TAG = '2022.08.11'
 
 def _get_git_version():
     plugin_path = os.path.realpath(os.path.dirname(__file__))
